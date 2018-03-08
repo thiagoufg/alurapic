e65d69959db413130e9834a5d4b216767a47f066
Thiago Garcia <thiagoufg@gmail.com>
2018-03-08 10:02:47 -0300

    Pipe

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -3,10 +3,11 @@ import { Component, Input } from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'foto',
-    templateUrl: './foto.component.html'
+    templateUrl: './foto.component.html' 
 })
 export class FotoComponent {
 
-    @Input() titulo;
-    @Input() url;
+    @Input() titulo: string;
+    @Input() url: string;
+    descricao: string;
 }
