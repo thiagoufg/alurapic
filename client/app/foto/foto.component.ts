1dbcaf5385446f465c710a55d577da7ac9cb2ab0
Thiago Garcia <thiagoufg@gmail.com>
2018-03-08 10:28:54 -0300

    Form sem ngModel

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class FotoComponent {
 
-    @Input() titulo: string;
-    @Input() url: string;
-    descricao: string;
+    @Input() titulo: string = '';
+    @Input() url: string = '';
+    descricao: string = '';
 }
