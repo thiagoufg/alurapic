87235f85a1244187fac0284f47757963aeb95766
Thiago Garcia <thiagoufg@gmail.com>
2018-04-26 09:53:18 -0300

    Mensagens de erro e alerta

--- client/app/listagem/listagem.component.js
+++ client/app/listagem/listagem.component.js
@@ -14,21 +14,28 @@ var ListagemComponent = (function () {
     function ListagemComponent(service) {
         var _this = this;
         this.fotos = [];
+        this.mensagem = '';
         this.service = service;
-        service.lista()
-            .subscribe(function (fotos) { return _this.fotos = fotos; }, function (erro) { return console.log(erro); });
+        this.service
+            .lista()
+            .subscribe(function (fotos) {
+            _this.fotos = fotos;
+        }, function (erro) { return console.log(erro); });
     }
-    // código anterior omitido 
     ListagemComponent.prototype.remove = function (foto) {
         var _this = this;
-        this.service.remove(foto)
-            .subscribe(function (fotos) {
-            console.log('Foto removida com sucesso');
+        this.service
+            .remove(foto)
+            .subscribe(function () {
             var novasFotos = _this.fotos.slice(0);
             var indice = novasFotos.indexOf(foto);
             novasFotos.splice(indice, 1);
             _this.fotos = novasFotos;
-        }, function (erro) { return console.log(erro); });
+            _this.mensagem = 'Foto removida com sucesso';
+        }, function (erro) {
+            console.log(erro);
+            _this.mensagem = 'Não foi possível remover a foto';
+        });
     };
     ListagemComponent = __decorate([
         core_1.Component({
--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -6,33 +6,43 @@ import { FotoService } from '../foto/foto.service';
 @Component({
     moduleId: module.id,
     selector: 'listagem',
-    templateUrl: './listagem.component.html' 
+    templateUrl: './listagem.component.html'
 })
-export class ListagemComponent { 
+export class ListagemComponent {
 
     fotos: FotoComponent[] = [];
     service: FotoService;
+    mensagem: string = '';
 
     constructor(service: FotoService) {
+
         this.service = service;
-        service.lista()
-            .subscribe(
-                fotos => this.fotos = fotos,
-                erro => console.log(erro)
-            );
+        this.service
+            .lista()
+            .subscribe(fotos => {
+                this.fotos = fotos;
+            }, erro => console.log(erro));
+
     }
 
-    // código anterior omitido 
-    remove(foto: FotoComponent): void {
-        this.service.remove(foto)
-             .subscribe(
-                fotos => {
-                    console.log('Foto removida com sucesso');
+    remove(foto) {
+
+        this.service
+            .remove(foto)
+            .subscribe(
+                () => {
+
                     let novasFotos = this.fotos.slice(0);
                     let indice = novasFotos.indexOf(foto);
                     novasFotos.splice(indice, 1);
                     this.fotos = novasFotos;
+                    this.mensagem = 'Foto removida com sucesso';
                 },
-                erro => console.log(erro));
+                erro => {
+                    console.log(erro);
+                    this.mensagem = 'Não foi possível remover a foto';
+                }
+            );
+
     }
 }
